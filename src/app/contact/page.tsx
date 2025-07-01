124c11d5d3c0ad294e3c2afd714bbd6fffa6c07e
snakeMax <38764043+snakeMax@users.noreply.github.com>
2025-07-01 14:52:48 +0200

    added more text and content
    to all pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,31 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 export default function page() {
   return (
     <div className="flex flex-col items-center h-screen">
       <div className='bg-black/80 w-full max-w-[800px] h-full rounded-xl shadow-[0_0_40px_rgba(0,255,0,0.6)] border border-green-500/20 p-8'>
         <div className="flex flex-col items-center">  
-          <h1>Contact</h1>
+          <h1 className="text-4xl font-bold my-4 text-white">Contact me</h1>
+
+          <p className="text-xl my-4 text-white">
+            Here are my socials and contact information
+          </p>
+
+          <div className="flex flex-row items-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35 w-full">
+            <a href="https://github.com/snakeMax" className="flex flex-row items-center gap-4">
+              <FaGithub size={80} />
+              <p>GitHub</p>
+            </a>
+          </div>
+
+          <div className="flex flex-row items-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35 w-full">
+            <a href="https://www.linkedin.com/in/chloeknutsen/" className="flex flex-row items-center gap-4">
+              <FaLinkedin size={80} />
+              <p>LinkedIn</p>
+            </a>
+          </div>
+
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,34 +14,34 @@ export default function Home() {
           <Image src="/pfp.png" alt="Pfp" width={100} height={100} className="rounded-2xl"/>
           <h1>Chloe Knutsen</h1>
         </div>
-        <div className="flex flex-row items-center gap-4">
+        <div className="flex flex-row items-center gap-4 mt-8 mb-4">
           <h3>Full Stack Developer</h3>
-          <h3>Software Enguneer</h3>
+          <h3>Software Engineer</h3>
           <h3>AI Data Analyst</h3>
         </div>
 
-        <section id="about" className="flex flex-row items-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35">
+        <section id="about" className="flex flex-row justify-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35 w-full">
           <a href="/about" className="flex flex-row items-center gap-4">
             <MdOutlineEmojiPeople size={80} />
-            <p>Placeholder text for future editorial purposes</p>
+            <p>Information about me</p>
           </a>
         </section>
-        <section id="projects" className="flex flex-row items-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35">
+        <section id="projects" className="flex flex-row justify-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35 w-full">
           <a href="/projects" className="flex flex-row items-center gap-4">
             <MdContactPage size={80} />
-            <p>Placeholder text for future editorial purposes</p>
+            <p>My projects</p>
           </a>
         </section>
-        <section id="portfolio" className="flex flex-row items-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35">
+        <section id="portfolio" className="flex flex-row justify-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35 w-full">
           <a href="/portfolio" className="flex flex-row items-center gap-4">
             <FaBook size={75} />
-            <p>Placeholder text for future editorial purposes</p>
+            <p>Portfolio</p>
           </a>
         </section>
-        <section id="contact" className="flex flex-row items-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35">
+        <section id="contact" className="flex flex-row justify-center gap-4 my-8 text-xl border-2 border-white rounded-2xl px-4 py-2 bg-gray-800 h-35 w-full">
           <a href="/contact" className="flex flex-row items-center gap-4">
             <IoMdContacts size={80} />
-            <p>Placeholder text for future editorial purposes</p>
+            <p>Contact info</p>
           </a>
         </section>
         
