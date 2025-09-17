837abd8b5e2eb3d8a3c80ba18604fb37931dcdeb
snakeMax <38764043+snakeMax@users.noreply.github.com>
2025-09-17 15:28:37 +0200

    build

--- src/api/testEndpoint.js
+++ src/api/testEndpoint.js
@@ -1,14 +1,14 @@
-async function testEndpoint(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
+// async function testEndpoint(url) {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+// }
 
-async function testEndpoint2(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  data.map(d => console.log(d));
-  return data;
-}
+// async function testEndpoint2(url) {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   data.map(d => console.log(d));
+//   return data;
+// }
 
-console.log(testEndpoint2("http://localhost:5044/api/projects"));
+// console.log(testEndpoint2("http://localhost:5044/api/projects"));
